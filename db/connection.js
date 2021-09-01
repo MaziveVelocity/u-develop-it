14d497a11937b86da6c34444cb9f121c60fbff99
Cayman Gill <cayman.g@hotmail.com>
2021-09-01 19:39:41 -0400

    resturctered code to be more orginized and added additional tables

---
+++ db/connection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2');
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Csg@0605974783',
+        database: 'election',
+    },
+    console.log('Connected to the election database.')
+);
+
+module.exports = db;
